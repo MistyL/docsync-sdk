e5b77f34ee03f619d8c0cb600dc4f959bd67f70a
MistyL <liangmengmeng@qiniu.com>
2016-06-03 10:14:38 +0800

    init project and finish webserviceclient

---
+++ app/src/main/java/docsync/qinu/com/qiniu_docsync_sdk/MainActivity.java
@@ -0,0 +1,13 @@
+package docsync.qinu.com.qiniu_docsync_sdk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
